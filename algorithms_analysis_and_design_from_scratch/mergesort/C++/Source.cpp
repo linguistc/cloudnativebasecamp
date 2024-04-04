e5b42e9433087834540204374233aea26860cd5e
iahmed <ahmadsameh1@proton.me>
2024-04-04 23:28:28 +0200

    add C++ implementation for merge sort algoritm.

---
+++ algorithms_analysis_and_design_from_scratch/mergesort/C++/Source.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+using namespace std;
+
+void merge(int array[], int start, int midPoint, int end)
+{
+	int left_lenght = midPoint - start + 1;
+	int right_lenght = end - midPoint;
+
+	int* left_array = new int[left_lenght];
+	int* right_array = new int[right_lenght];
+
+	int i, j, k;
+
+	for (i = 0; i < left_lenght; ++i)
+		left_array[i] = array[start + i];
+
+	for (i = 0; i < right_lenght; ++i)
+		right_array[i] = array[midPoint + 1 + i];
+
+	i = 0, j = 0, k = start;
+
+	while (i < left_lenght && j < right_lenght)
+	{
+		if (left_array[i] < right_array[j])
+		{
+			array[k] = left_array[i];
+			++i;
+		}
+		else {
+			array[k] = right_array[j];
+			++j;
+		}
+		++k;
+	}
+
+	while (i < left_lenght)
+	{
+		array[k] = left_array[i];
+		++i, ++k;
+	}
+	while (j < right_lenght)
+	{
+		array[k] = right_array[j];
+		++j, ++k;
+	}
+
+	delete left_array, right_array;
+
+}
+
+void mergeSort(int array[], int start, int end)
+{
+	if (end <= start) return;
+	int midPoint = (end + start) / 2;
+
+	mergeSort(array, start, midPoint);
+	mergeSort(array, midPoint + 1, end);
+	merge(array, start, midPoint, end);
+}
+
+void readArr(int arr[], int n)
+{
+	for (int i = 0; i < n; ++i)
+	{
+		cout << "Element " << i + 1 << ": ";
+		cin >> arr[i];
+	}
+}
+
+void printArr(int arr[], int n)
+{
+	cout << "[";
+	for (int i = 0; i < n; ++i)
+	{
+		cout << arr[i];
+		if (i != n - 1) cout << ", ";
+	}
+	cout << "]\n";
+}
+
+int main()
+{
+	int n;
+	cout << "How many Elements to sort? ";
+	cin >> n;
+	int* arr = new int[n];
+	readArr(arr, n);
+	mergeSort(arr, 0, n - 1);
+	printArr(arr, n);
+
+
+	return 0;
+}
