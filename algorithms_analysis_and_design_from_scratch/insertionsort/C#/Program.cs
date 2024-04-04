7153c2680c33f2d61427a72dbabaec832e4bf069
iahmed <ahmadsameh1@proton.me>
2024-04-04 22:50:54 +0200

    add C# implementation for insertion sort algoritm.

---
+++ algorithms_analysis_and_design_from_scratch/insertionsort/C#/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+    internal class Program
+    {
+        static void InsertionSort(int[] arr)
+        {
+            int i = 1, j, key;
+            for(; i < arr.Length; ++i) 
+            {
+                j = i - 1;
+                key = arr[i];
+                while(j >= 0 && key < arr[j])
+                {
+                   
+                    arr[j + 1] = arr[j];
+                    
+                    --j;
+                }
+                arr[j+1] = key;
+            }
+        }
+
+        static void PrintArray(int[] arr)
+        {
+            Console.Write("[");
+            for(int i = 0; i < arr.Length; ++i)
+            {
+                Console.Write(arr[i]);
+                if(i != arr.Length - 1)
+                    Console.Write(", ");
+            }
+            Console.Write("]");
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 4, 2, 29, 10, 7 };
+            InsertionSort(arr);
+            PrintArray(arr);
+            Console.ReadKey();
+        }
+    }
