3035908f7cef15ac9de398d31794a01e9ed3b01f
iahmed <ahmadsameh1@proton.me>
2024-04-23 18:24:19 +0200

    Adding Huffman Coding algorithm implemented in C#

---
+++ algorithms_analysis_and_design_from_scratch/Greedy-Huffman/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string msg = "internet";
+        clsHuffman huff = new clsHuffman(msg);
+
+        foreach(char key in huff.htCodes.Keys) 
+        {
+            Console.WriteLine(key + ": " + huff.htCodes[key]);
+        }
+    }
+}
+
+
