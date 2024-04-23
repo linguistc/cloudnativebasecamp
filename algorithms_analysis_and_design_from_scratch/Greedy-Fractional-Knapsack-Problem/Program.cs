2d43afbe4feef6f269f70c5fdc578d9907c4f624
iahmed <ahmadsameh1@proton.me>
2024-04-23 21:35:32 +0200

    Adding Primary implementation of Fractional Knapsack Problem

---
+++ algorithms_analysis_and_design_from_scratch/Greedy-Fractional-Knapsack-Problem/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Greedy_Fractional_Knapsack_Problem
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            int[] profits = { 4, 9, 12, 11, 6, 5 };
+            int[] weights = { 1, 2, 10, 4, 3, 5 };
+
+            List<clsItem> items = new List<clsItem>();
+
+            for (int i = 0; i < profits.Length; ++i)
+            {
+                clsItem newItem = new clsItem(profits[i], weights[i], "#" + i.ToString());
+                items.Add(newItem);
+            }
+
+            clsSorting s = new clsSorting();
+            s.MergeSort(items, 0, items.Count - 1);
+
+            clsPrint p = new clsPrint();
+            p.print_array(items);
+
+            int j = 0;
+            clsKnapsack bag = new clsKnapsack(12);
+            while (bag.currentCapacity < bag.maxCapacity && j < items.Count)
+                bag.AddItem(items[j++]);
+
+            p.PrintItems(bag);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ algorithms_analysis_and_design_from_scratch/Greedy-Fractional-Knapsack-Problem/clsKnapsack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Greedy_Fractional_Knapsack_Problem
+{
+ 
+    public class clsItem
+    {
+        public string name;
+        public float profit;
+        public int weight;
+        public float ratio;
+
+        public clsItem() { }
+        public clsItem(int profit, int weight, string name)
+        {
+            this.name = name;
+            this.profit = profit;
+            this.weight = weight;
+            ratio = (float)profit / weight;
+        }
+    }
+
+    public class clsKnapsack
+    {
+        public int maxCapacity, currentCapacity;
+        public float totalProfit;
+        public List<clsItem> lItems = new List<clsItem>();
+
+        public clsKnapsack(int maxCapacity) 
+        {
+            this.maxCapacity = maxCapacity;
+            currentCapacity = 0;
+            totalProfit = 0;
+        }
+
+        public void AddItem(clsItem newItem)
+        {
+            if (newItem.weight > maxCapacity - currentCapacity)
+            {
+                int diff = maxCapacity - currentCapacity;
+                newItem.weight = diff;
+                newItem.profit = diff * newItem.ratio;
+            }
+
+            lItems.Add(newItem);
+            currentCapacity += newItem.weight;
+            totalProfit += newItem.profit;
+        }
+
+    }
+}
